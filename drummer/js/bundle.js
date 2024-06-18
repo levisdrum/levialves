9b07bd75d5d7c5785173d27223a5f75f44fae01e
Levi Oliveira <levi.alves@grupoboticario.com.br>
2024-06-18 01:39:47 -0300

    atualiza path das imagens

--- drummer/js/bundle.js
+++ drummer/js/bundle.js
@@ -10376,6 +10376,47 @@ var modalNav = function () {
 modalNav.init();
 // FUNCTION NAV
 var nav = function () {
+
+  var functions = {
+
+    nav_open: function () {
+      $('.nav').addClass('ativo');
+      $('.open').css('opacity', '0');
+    },
+    nav_close: function () {
+      $('.nav').removeClass('ativo');
+      $('.open').css('opacity', '1');
+    }
+
+  };
+
+  return {
+
+    plugins: function () {
+
+      $(document).ready(function () {
+
+        $('.open').click(function () {
+          functions.nav_open();
+        });
+        $('.close, .btn-nav').click(function () {
+          functions.nav_close();
+        });
+      });
+    },
+
+    //init nav
+    init: function () {
+      this.plugins();
+    }
+
+  };
+}();
+
+//init object
+nav.init();
+// FUNCTION NAV
+var nav = function () {
   var functions = {
     menu_toggle: function () {
       // Alternar visibilidade com animação
@@ -10428,45 +10469,4 @@ var nav = function () {
 }();
 
 // Init object
-nav.init();
-// FUNCTION NAV
-var nav = function () {
-
-  var functions = {
-
-    nav_open: function () {
-      $('.nav').addClass('ativo');
-      $('.open').css('opacity', '0');
-    },
-    nav_close: function () {
-      $('.nav').removeClass('ativo');
-      $('.open').css('opacity', '1');
-    }
-
-  };
-
-  return {
-
-    plugins: function () {
-
-      $(document).ready(function () {
-
-        $('.open').click(function () {
-          functions.nav_open();
-        });
-        $('.close, .btn-nav').click(function () {
-          functions.nav_close();
-        });
-      });
-    },
-
-    //init nav
-    init: function () {
-      this.plugins();
-    }
-
-  };
-}();
-
-//init object
 nav.init();
